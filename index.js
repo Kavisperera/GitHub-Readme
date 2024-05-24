4e874ff769f17e202ff61666538743f910428334
kavisperera <kavindayap57@gmail.com>
2024-05-24 16:41:02 +0530

    2024-05-24T16:41:02+05:30

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
 
-const FILE_PATH ='./data.json';
+const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const makeCommit = (n) => {
+    if (n === 0) return simpleGit().push();
+    
+    const x = Math.floor(Math.random() * 55);
+    const y = Math.floor(Math.random() * 7);
+    
+    const DATE = moment().subtract(1, 'y').add(1, 'd')
+                                 .add(x, 'w').add(y, 'd').format();
+    
+    const data = { date: DATE };
+    console.log(DATE);
+    
+    jsonfile.writeFile(FILE_PATH, data, () => {
+        simpleGit().add(['data.json', 'index.js']).commit(DATE, {'--date': DATE },
+        makeCommit.bind(this, --n));
+    });
+};
 
-const data = {
-    date:DATE
-}
-
-jsonfile.writeFile(FILE_PATH,data);
-
-//------ git commit --date=""
-simpleGit().add([FILE_PATH]).commit(DATE,{'--date': DATE }).push();
+makeCommit(100);
